ea6d9cb84e7210264d71ba51f4928dcaa5d2a50f
silkyspider <james.hamilton.2001@outlook.com>
2024-02-13 01:33:10 +0000

    added user choice of predefined setting

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,90 @@
 #include "App.hpp"
 
+
+void redDeathSmall(int& types, int& size, int& count, float& resistance, float& innerRadius, float& step, std::vector<std::vector<float>>& attractions, int& seed);
+
+
 int main()
 {
     int width = 800;
     int height = 800;
     int fpsTarget = 0;
 
-    InitWindow(width, height, "Particle Life");
-    SetTargetFPS(fpsTarget);
+    int option;
 
-    App app(width, height, fpsTarget);
+    int types;
+    int size;
+    int count;
+    float resistance;
+    float innerRadius;
+    float step;
+    std::vector<std::vector<float>> attractions;
+    int seed;
 
-    ParticleLife particleLife(
-        3,                              // types
-        16,                             // size
-        2024,                            // count
-        0.0025f,                        // resistance
-        0.5f,                           // innerRadius
-        0.0002f,                        // step
-        { {  0.02f, -0.01f,  0.04f },
-          { -0.02f,  0.01f, -0.02f },   // attractions
-          { -0.01f,  0.03f,  0.02f } },
-        1234                            // seed
-    );
-
-    while (!WindowShouldClose())
+    while (true)
     {
-        app.update(particleLife);
-        app.draw(particleLife);
-    }
+        std::cin >> option;
 
-    CloseWindow();
+        switch(option)
+        {
+            case 1:
+                redDeathSmall(types, size, count, resistance, innerRadius, step, attractions, seed);
+                break;
+            default:
+                continue;
+                break;
+        }
+
+        InitWindow(width, height, "Particle Life");
+        SetTargetFPS(fpsTarget);
+
+        App app(width, height, fpsTarget);
+
+        ParticleLife particleLife(
+            types,                             
+            size,                             
+            count,                            
+            resistance,                       
+            innerRadius,                      
+            step,                             
+            attractions,                      
+            seed                              
+        );
+
+        while (!WindowShouldClose())
+        {
+            app.update(particleLife);
+            app.draw(particleLife);
+        }
+
+        CloseWindow();
+        
+    }
 
     return 0;
 
+}
+
+void redDeathSmall(int& types, int& size, int& count, float& resistance, float& innerRadius, float& step, std::vector<std::vector<float>>& attractions, int& seed)
+{
+    types       = 5;
+    size        = 16;
+    count       = 512;
+    resistance  = 0.0025f;
+    innerRadius = 0.5f;
+    step        = 0.0025f;
+
+    attractions.resize(types, std::vector<float>(types, 0.0f));
+
+    attractions[0][0] = 0.06f;
+    for (int i = 1; i < types; i++)
+        attractions[0][i] = 0.04f;
+    for (int i = 1; i < types; i++)
+        attractions[i][0] = -0.04f;
+    for (int i = 1; i < types; i++)
+        for (int j = 0; j < types; j++)
+            attractions[i][j] = -0.01f + (i+j) % 4 * 0.01f;
+
+    seed = 1234;
+    
 }
